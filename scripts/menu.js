247625374ec01f08489c7f30d52a830fa0e3efd7
Bernardo Marques <bernardohmarques@gmail.com>
2025-02-02 13:39:21 -0300

    add animation

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,4 +1,4 @@
-const mainnav = document.querySelector(".navigation");
+const mainnav = document.querySelector("#animated");
 const hambutton = document.querySelector("#menu");
 
 hambutton.addEventListener("click", () => {
